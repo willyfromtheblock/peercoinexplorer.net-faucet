f887703ac91185cc97e69dca601b02aea77f62ef
Willy <bananenwilly@users.noreply.github.com>
2020-03-01 21:12:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -203,7 +203,7 @@ class App extends Component {
                     onClick={() => this.showModal()}
                     className="btn btn-secondary donate_addr"
                   >
-                    PA3VZmupxdsX5TuS1PyXZPsbbhZGT2htPz
+                    PPXMXETHJE3E8k6s8vmpDC18b7y5eKAudS
                   </button>
                 </p>
               </div>
